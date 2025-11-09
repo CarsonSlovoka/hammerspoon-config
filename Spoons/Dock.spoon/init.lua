7e7a5fe61aee3a6aa815ccc95456eedc9328c2ad
Carson <jackparadise520a@gmail.com>
2025-11-09 22:29:53 +0800

    ( 可以用模組化的方式來拆開寫設定 )

---
+++ Spoons/Dock.spoon/init.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+hs = hs
+
+function M.Test()
+  hs.alert.show("load my Dock.")
+end
+
+return M
--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 hs = hs -- 減少未定義的警告
 
+hs.loadSpoon("Dock")
+-- 如此在它的終端機，可以直接使用
+-- spoon.Dock.hideDock()
+
 -- hs.hotkey.bind({ "alt" }, "R", function()
 --   hs.reload()
 -- end)
