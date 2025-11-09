897d6f3f795058aae96fef69d84a9c7291d74614
Carson <jackparadise520a@gmail.com>
2025-11-10 00:01:51 +0800

    ( 可以用package.path的方法, require其它lua路徑 )

--- init.lua
+++ init.lua
@@ -1,5 +1,14 @@
 hs = hs -- 減少未定義的警告
 
+local hammerspoon_config_dir = os.getenv("HOME") .. '/.hammerspoon/'
+package.path = package.path ..
+    ';' .. hammerspoon_config_dir .. 'lua/?.lua'
+-- hs.alert.show(package.path)
+-- print(package.path)
+
+local test = require("test")
+test.test()
+
 hs.loadSpoon("Dock")
 -- 如此在它的終端機，可以直接使用
 -- spoon.Dock.hideDock()
---
+++ lua/test.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+hs = hs
+
+function M.test()
+  hs.alert.show("[lua.test.lua] test")
+end
+
+return M
