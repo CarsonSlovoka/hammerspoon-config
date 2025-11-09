daac37c88c797a17d69c6da0f8ebf9289754a2c5
Carson <jackparadise520a@gmail.com>
2025-11-10 00:59:23 +0800

    🚀 做到類似fuzzel的效果

--- init.lua
+++ init.lua
@@ -32,11 +32,37 @@ local fuzzelList = {
     subText = "What a lot of choosing there is going on here!",
     image = hs.image.imageFromName("NSComputer"),
   },
+
+  {
+    text = "hammerspoon reload",
+    -- cmd = function() hs.reload() end -- ❌ cannot be converted into a proper NSObject, 因此沒辦法直接用function, 只能額外用table去找對應要執行的函數
+    cmdName = "hammerspoon reload"
+  },
+  {
+    text = "list hs.image",
+    cmdName = "list hs.image"
+  }
 }
 
+local cmdTable = {
+  ["hammerspoon reload"] = function()
+    hs.reload()
+  end,
+  ["list hs.image"] = function()
+    print(hs.image.systemImageNames)
+    -- local searchIcon = hs.image.imageFromName("NSSearchFieldTemplate") -- 載入搜尋圖示
+  end
+}
 
 local function completionFn(choice)
   if not choice then return end
+  if choice.cmdName then
+    local cmdFunc = cmdTable[choice.cmdName]
+    if cmdFunc then
+      cmdFunc()
+    end
+    return
+  end
   hs.application.launchOrFocus(choice.path)
 end
 
