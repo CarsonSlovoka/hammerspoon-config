9427aa2dbfe582fe863d25a9ce82b143434f4be1
Carson <jackparadise520a@gmail.com>
2025-11-17 23:12:49 +0800

    新增image

--- Spoons/Window.spoon/init.lua
+++ Spoons/Window.spoon/init.lua
@@ -1,12 +1,24 @@
 local M = {}
 
+local function imgFrom(name)
+  -- https://github.com/CarsonSlovoka/hammerspoon-config/tree/3ebc93f/assets/img
+  local path = os.getenv("HOME") .. '/.hammerspoon/assets/img/' .. name
+  if hs.fs.displayName(path) then
+    print(path)
+    return hs.image.imageFromPath(path)
+  end
+  return nil
+end
+
 function M.selectWindow()
   local list = {}
   for _, win in ipairs(hs.window.allWindows()) do
+    local appName = win:application():name()
     table.insert(list, {
       win = win,
       text = win:title(),
-      subText = win:application():name(),
+      subText = appName,
+      image = imgFrom(string.gsub(appName, " ", "") .. ".icns")
     })
   end
   local chooser = hs.chooser.new(function(choice)
