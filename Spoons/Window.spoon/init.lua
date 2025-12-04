f8396a59ea1a6eae315b193b783e6c005f4090ed
Carson <jackparadise520a@gmail.com>
2025-12-04 13:55:40 +0800

    如果bundleID是com.apple, 就會嘗試找尋系統的icns

--- Spoons/Window.spoon/init.lua
+++ Spoons/Window.spoon/init.lua
@@ -10,6 +10,11 @@ local function imgFrom(name)
   return nil
 end
 
+local function imageFromSystemApp(appName)
+  return hs.image.imageFromPath(string.format("/System/Applications/%s/Contents/Resources/AppIcon.icns", appName)) or
+      hs.image.imageFromPath(string.format("/System/Applications/%s/Contents/Resources/AppIconLoc.icns", appName)) -- 本地化的icns
+end
+
 
 ---@param opt table?
 function M.selectWindow(opt)
@@ -21,13 +26,20 @@ function M.selectWindow(opt)
 
     -- bundleID 只對com.apple的項目取，這通常都是系統的工具，如果是第三方的之後的第三碼是一個id, 識別那個也不好
     -- 只抓是com.apple的bundleID, 並且不要前面的com.apple
-    local bundleID = string.match(app:bundleID(), "^com%.apple%.(.+)") or ""
+    local bundleIDLast = string.match(app:bundleID(), "^com%.apple%.(.+)") or ""
+    local image
+    if bundleIDLast == "" then
+      image = imgFrom(string.gsub(appName, " ", "") .. ".icns")
+    else
+      image = imageFromSystemApp(bundleIDLast .. ".app")
+    end
 
     table.insert(list, {
       win = win,
       text = win:title(),
-      subText = appName .. " " .. bundleID, -- 加上bundleID比較方便用英文搜尋
-      image = imgFrom(string.gsub(appName, " ", "") .. ".icns")
+      subText = appName .. " " .. bundleIDLast, -- 加上bundleID比較方便用英文搜尋
+      -- subText = appName .. " " .. app:bundleID(),
+      image = image
     })
   end
   local chooser = hs.chooser.new(function(choice)
