6b5b9f494f314b300b417e590efda5e1cc0c40f3
Carson <jackparadise520a@gmail.com>
2025-12-04 13:36:18 +0800

    新增bundleID顯示於subText

--- Spoons/Window.spoon/init.lua
+++ Spoons/Window.spoon/init.lua
@@ -16,11 +16,17 @@ function M.selectWindow(opt)
   opt = opt or {}
   local list = {}
   for _, win in ipairs(hs.window.allWindows()) do
-    local appName = win:application():name()
+    local app = win:application()
+    local appName = app:name()
+
+    -- bundleID 只對com.apple的項目取，這通常都是系統的工具，如果是第三方的之後的第三碼是一個id, 識別那個也不好
+    -- 只抓是com.apple的bundleID, 並且不要前面的com.apple
+    local bundleID = string.match(app:bundleID(), "^com%.apple%.(.+)") or ""
+
     table.insert(list, {
       win = win,
       text = win:title(),
-      subText = appName,
+      subText = appName .. " " .. bundleID, -- 加上bundleID比較方便用英文搜尋
       image = imgFrom(string.gsub(appName, " ", "") .. ".icns")
     })
   end
