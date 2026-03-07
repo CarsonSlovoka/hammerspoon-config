eeb86c7ae578ea404a0bb2d19bf31b15d2e88f5d
Carson <jackparadise520a@gmail.com>
2026-03-07 15:28:24 +0800

    ( 預設瀏覽器改用Safari )
    ( 如果不習慣它的鍵位也可以改鍵 )

--- lua/config.lua
+++ lua/config.lua
@@ -1,7 +1,8 @@
 local M = {
   -- 設定主終端機、主瀏覽器(影響清單出現的順序、熱鍵開啟的對像)
   termianl = "Ghostty", -- kitty, Ghostty
-  browser = "Firefox"   -- Firefox, Safari
+  browser = "Safari"    -- Firefox, Safari
+  -- Note: Safari的鍵位是可以調整的，可以透過 `設定 > 鍵盤 > App快速鍵` 去指派快速鍵. Caution: 名稱要和本地語言對應
 }
 
 return M
